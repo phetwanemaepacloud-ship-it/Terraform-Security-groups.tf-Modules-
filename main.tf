1c2d5c81fc33c6306aff654bc815b3eb13bf8840
phetwanemaepacloud-ship-it <phetwanemaepacloud@gmail.com>
2026-08-15 10:18:47 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ tags {
 resource "aws_security_group" "alb_security_group" {
   name         = "${var.environment]-${var.project_name}-alb-sg" 
   description  = "HTTP/HTTPS from internet"
-  vpc_id       = 
+  vpc_id       = aws_vpc.vpc.id
 
 ingress {
   description = "HTTP"
@@ -36,3 +36,16 @@ from_port   = 443
 to_port     = 443 
 protocol    = "tcp" 
 cidr_block  = ["0.0.0.0/0"] ID.
+
+
+egress (
+  from_port = 0 
+  to_port   = 0
+  protocol = -1
+  cidr_blocks = ["0.0.0.0/0"]
+} 
+
+
+tags = {
+Name = "${var.environment)-${var.project_name}-alb-sg"
+} 
