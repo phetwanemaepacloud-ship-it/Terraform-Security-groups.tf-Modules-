d1ecc5d25a72c8fb67b960f2bdf53207cb6b037e
phetwanemaepacloud-ship-it <phetwanemaepacloud@gmail.com>
2026-08-15 10:31:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,3 +49,37 @@ egress (
 tags = {
 Name = "${var.environment)-${var.project_name}-alb-sg"
 } 
+
+
+# Security group for Application Load Balancer 
+resource "aws_security_group" "alb_security_group" {
+  name        = "${var.environment)-${var.project_name}-alb-sg" 
+  description = "HTTP/HTTPS from internet"
+  vpc_id      = var.vpc_id
+  
+ingress {
+  description = "HTTP 
+  from_port   = 80 
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+ingress {
+  description = "HTTPS"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+
+egress {
+  from_port   = 0
+  to_port     = 0
+  protocol    = -1
+  cidr_blocks = ["0.0.0.0/0"]
+} 
+
+tags =
+  Name = "${var.environment)-${var.project_name}-alb-sg"
+  } 
+} 
